f96fd4abe3f2d3efafb4379ce98fab3649ace4cf
John Dominion <jdominion123@gmail.com>
2024-09-10 12:51:43 +0100

    addition of docstrings to explain the improvement of the logger setup

--- src/log.py
+++ src/log.py
@@ -1,11 +1,18 @@
-""" This logger was created is an improvement on the default logger that comes with flask"""
+""" This logger was created is an improvement
+on the default logger that comes with flask"""
+
 import logging
 from logging.handlers import RotatingFileHandler
 import os
 
 
 def setup_logger(app):
-    """ The function  implements improvements on flask's default logger. """
+
+    """ The function  implements improvements on flask's default logger.
+    its sets the logging level,specifiesthe format of log
+    the log messages,ensures that log file size don't
+     exceed 10kb and stores upto 10 log files  """
+
     # Ensuring log directory exists
     if not os.path.exists('logs'):
         os.mkdir('logs')
