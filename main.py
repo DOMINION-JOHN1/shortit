9a195aef239e9f26785009adb577dddd6d300a64
John Dominion <jdominion123@gmail.com>
2024-10-03 11:16:32 +0100

    added more docstrings to each route

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ app = Flask(__name__)
 
 @app.route('/get_all_urls', methods=['GET'])
 def all_urls():
+    """This function returns all the url stored in the database"""
     return jsonify(get_all_urls())
 
 
 @app.route('/urls/<string:url_id>', methods=['GET'])
 def one_url(url_id):
+    """This function only returns on url stored in the database using the url id"""
     # Call the function and return the result
     return jsonify(get_one_url(url_id))
 
 
 @app.route('/urls', methods=['POST'])
 def short_url():
+    """ This function return short url when posted with a url"""
     return jsonify(create_short_url())
 
 
